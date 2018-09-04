c8a0f29c6cb1600163353ae3cc6f915639623d8d
Ildar Karimov <ildarkarimov22@gmail.com>
2018-09-05 03:38:32 +0500

    main components created, added some routes, added some styles

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>
+      Not found
+    </h1>
+    <p>
+      Ooops, we couldn't find what you are looking for. Why don't you
+      <router-link :to="{name:'Home'}">
+        go home instead?
+      </router-link>
+    </p>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'PageNotFound'
+  }
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 6rem;
+  }
+  p {
+    font-size: 3.5rem;
+    font-weight: 100;
+  }
+  div {
+    text-align: center;
+  }
+</style>
