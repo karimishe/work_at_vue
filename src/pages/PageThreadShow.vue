562347cc5239eebdd9e6b92f1ef12eb0668f2a29
karimov_i <karimov_i@ufanet.ru>
2018-09-05 17:35:17 +0500

    postEditor created, post styles moved to global styles

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,17 +2,23 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
+    <PostEditor
+      @save="addPost"
+      :threadId="id"
+    />
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
   import PostList from '@/components/PostList'
+  import PostEditor from '@/components/PostEditor'
 
   export default {
     name: 'ThreadShow',
     components: {
-      PostList
+      PostList,
+      PostEditor
     },
     props: {
       id: {
@@ -23,15 +29,22 @@
 
     data () {
       return {
-        thread: sourceData.threads[this.id],
-        posts: sourceData.posts,
-        users: sourceData.users
+        thread: sourceData.threads[this.id]
       }
     },
     computed: {
       posts () {
         const postIds = Object.values(this.thread.posts)
         return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']))
+      }
+    },
+    methods: {
+      addPost (eventData) {
+        const post = eventData.post
+        const postId = eventData.post['.key']
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
       }
     }
   }
