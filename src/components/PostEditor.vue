0abb81feda9b50b8f715de9eb56799a622238529
karimishe <ildarkarimov22@gmail.com>
2018-09-07 19:10:29 +0500

    some refactoring and icons connecting

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -35,9 +35,7 @@
       save () {
         const post = {
           text: this.text,
-          publishedAt: Math.floor(Date.now() / 1000),
-          threadId: this.threadId,
-          userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2'
+          threadId: this.threadId
         }
         this.$emit('save', {post})
 
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -28,16 +28,13 @@
         type: String
       }
     },
-
-    data () {
-      return {
-        thread: this.$store.state.threads[this.id]
-      }
-    },
     computed: {
       posts () {
         const postIds = Object.values(this.thread.posts)
         return Object.values(this.$store.state.posts).filter(post => postIds.includes(post['.key']))
+      },
+      thread () {
+        return this.$store.state.threads[this.id]
       }
     }
   }
