c917d9b444ee08a149f2c0bd09f04a0e901b17a3
karimishe <ildarkarimov22@gmail.com>
2018-09-11 17:06:43 +0500

    using dotenv variables

--- src/main.js
+++ src/main.js
@@ -14,12 +14,12 @@ Vue.config.productionTip = false
 
 // Initialize Firebase
 const config = {
-  apiKey: 'AIzaSyC7qKdkeODtFqI9Zp_rqQ6aR5wPpx3K5go',
-  authDomain: 'vue-forum-test-work.firebaseapp.com',
-  databaseURL: 'https://vue-forum-test-work.firebaseio.com',
-  projectId: 'vue-forum-test-work',
-  storageBucket: 'vue-forum-test-work.appspot.com',
-  messagingSenderId: '461782690012'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 firebase.initializeApp(config)
 
